eac21960f15361b0e7307508a095ec97eb312563
Sahil Rajput <sahilrajput03@gmail.com>
2022-10-12 01:07:57 +0530

    fast

--- decentralized-exchange/contracts/MultiSigWallet.sol
+++ decentralized-exchange/contracts/MultiSigWallet.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 // import "hardhat/console.sol";
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
 
 // Our Token Registry
 contract Dex {
@@ -12,7 +13,7 @@ contract Dex {
 
 	mapping(bytes32 => Token) public tokens;
 	bytes32[] public tokenList;
-	mapping(address => mapping(bytes32 => uint)) public traderBalances;
+	mapping(address => mapping(bytes32 => uint)) public traderBalances; // traderAddress => ticker => balance
 	address public admin;
 
 	constructor() public {
@@ -24,9 +25,21 @@ contract Dex {
 		tokenList.push(ticker);
 	}
 
-	// fn to send tokens
-	function deposit(){
-		
+	// fn to send tokens - consider `mohit` calls this fn(2, 'SIB') thus he means to shift from SIB account to this wallet say DEX.
+	function deposit(uint amount, bytes32 ticker) tokenExists(ticker) external {
+		IERC20(tokens[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+		traderBalances[msg.sender][ticker] += amount;
+	}
+
+	function withdraw(uint amount, bytes32 ticker) tokenExists(ticker) external {
+		require(traderBalances[msg.sender][ticker] >= amount, 'balance too low');
+		traderBalances[msg.sender][ticker] -= amount;
+		IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+	}
+
+	modifier tokenExists(bytes32 ticker){
+		require(tokens[ticker].tokenAddress != address(0), 'this token does not exist');
+		_;
 	}
 
 	modifier onlyAdmin(){
@@ -35,5 +48,5 @@ contract Dex {
 	}
 }
 
-// creating a wallet in our smart contract
+// Creating a wallet in our smart contract
 // Before a trader are able to our decentralized exchange they need to first send ethereum token to our smart contract. So we're gonna create two fns, a fn to receive ethereum tokens and another fn to withdraw these tokens. And we also need to keep track how many tokens are send by which address, so for that we're gonna need a mapping i.e., `traderBalances` which is address => bytes32 => uint (balanceOfEachTrader).
