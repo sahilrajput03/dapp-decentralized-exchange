98df9273c0fbb40f5662d43e4ab4b4c2c423ee2c
Sahil Rajput <sahilrajput03@gmail.com>
2022-10-18 19:38:04 +0530

    Merge project of client of multisig-wallet with its truffle setup in single directory

--- multisig-wallet/migrations/1_deploy_contract.js
+++ multisig-wallet/migrations/1_deploy_contract.js
@@ -39,7 +39,8 @@ export const networkName = '${networkName}'
 `.trim()
 
 	let data = JSON.stringify(config)
-	const TARGET_FILE_PATH = `./client/config/config-${networkName === 'development' ? 'local' : networkName}.ts`
+	const isLocal = (networkName === 'development' || networkName === 'develop')
+	const TARGET_FILE_PATH = `./config/config-${isLocal ? 'local' : networkName}.ts`
 	fs.writeFileSync(TARGET_FILE_PATH, JSON.parse(data))
 }
 
