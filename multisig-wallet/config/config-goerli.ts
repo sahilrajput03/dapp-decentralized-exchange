74d3ca92dde55d4acc31400ac418ba47d6adde56
Sahil Rajput <sahilrajput03@gmail.com>
2022-10-22 00:03:13 +0530

    up

--- multisig-wallet/config/config-goerli.ts
+++
@@ -1,3 +0,0 @@
-export const walletAddress = '0x3b8A107E6AfdE72Ed3172168B679B964596B2050'
-export const networkName = 'goerli'
-// (Alert: WORKS FOR GOERLI CONTRACTS ONLY) View txns @ goerli.etherscan.io: https://goerli.etherscan.io/address/0x3b8A107E6AfdE72Ed3172168B679B964596B2050
--- multisig-wallet/config/config-local.ts
+++
@@ -1,3 +0,0 @@
-export const walletAddress = '0xF59E12CA284D779DAa6C61cf8706b1D146069ffc'
-export const networkName = 'development'
-// (Alert: WORKS FOR GOERLI CONTRACTS ONLY) View txns @ goerli.etherscan.io: https://goerli.etherscan.io/address/0xF59E12CA284D779DAa6C61cf8706b1D146069ffc
